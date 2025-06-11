319f352008b1a0d6f07509c6af320d0825d4d6ac
HAIZAKURA <hhbilly99@gmail.com>
2025-06-11 17:53:27 +0900

    lint

--- scripts/new.js
+++ scripts/new.js
@@ -1,19 +1,18 @@
-import { writeFile, mkdir } from 'fs/promises'
-import path from 'path'
-import { program } from 'commander'
-import dayjs from 'dayjs'
+import { writeFile, mkdir } from 'fs/promises';
+import path from 'path';
+import { program } from 'commander';
+import dayjs from 'dayjs';
 
-program.arguments('<id> [name]').parse()
-const [id, name = id] = program.args
+program.arguments('<id> [name]').parse();
+const [id, name = id] = program.args;
 
-const time = dayjs().format('YYYY-MM-DDTHH:mm:ssZ')
-const content = 
-`---
+const time = dayjs().format('YYYY-MM-DDTHH:mm:ssZ');
+const content = `---
 title: ${name}
 create: ${time}
 ---
 
-`
-await mkdir(path.join(import.meta.dirname, '..', 'posts', id))
-const filePath = path.join(import.meta.dirname, '..', 'posts', id, 'index.md')
-writeFile(filePath, content, { encoding: 'utf-8' })
+`;
+await mkdir(path.join(import.meta.dirname, '..', 'posts', id));
+const filePath = path.join(import.meta.dirname, '..', 'posts', id, 'index.md');
+writeFile(filePath, content, { encoding: 'utf-8' });
