bd2bc6d70a775eac000859fc6d44c941e7404227
HAIZAKURA <hhbilly99@gmail.com>
2025-06-11 17:55:01 +0900

    add: custom layout

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <template #doc-after v-if="page.frontmatter.comment ?? true">
+      <div>
+        <h2 id="giscus">评论</h2>
+      </div>
+      <Giscus
+        id="giscus"
+        :key="page.relativePath"
+        v-bind="theme.giscus"
+        :theme="isDark ? 'transparent_dark' : 'light'"
+      />
+    </template>
+ </Layout>
+</template>
+
+<script setup lang="ts"> 
+import { ref, nextTick, watch, onMounted, computed } from 'vue';
+import { useData } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+import Giscus from '@giscus/vue'
+
+// import { data as posts } from '../posts.data';
+import type { ThemeConfig } from '.';
+
+const { Layout } = DefaultTheme;
+const { page, theme, frontmatter, isDark } = useData<ThemeConfig>();
+</script>
