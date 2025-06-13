a41f0ea0e0cc7032ed508c9a2e18ba8726602ff2
HAIZAKURA <hhbilly99@gmail.com>
2025-06-13 11:48:28 +0900

    add: comment

--- .vitepress/posts.data.ts
+++ .vitepress/posts.data.ts
@@ -5,6 +5,13 @@ const loader = createContentLoader('posts/*/index.md');
 
 export default {
   ...loader,
+  /**
+   * 异步加载文章数据
+   * 
+   * 从数据源加载文章信息 解析和处理返回一个按创建时间降序排列的文章数组
+   * 
+   * @returns {Promise<Post[]>} 返回一个Promise，解析为Post对象的数组
+   */
   async load(): Promise<Post[]> {
     const data = await loader.load();
     return data
@@ -21,7 +28,7 @@ export default {
         };
         return post;
       })
-      .sort((a, b) => b.create - a.create);
+      .sort((a, b) => b.create - a.create); // 按照创建时间降序排列
   },
 };
 
--- uno.config.ts
+++ uno.config.ts
@@ -1,22 +1,51 @@
 import { defineConfig, presetUno } from 'unocss';
 
+/**
+ * 将给定名称与一系列级别组合成带级别的名称数组
+ * 
+ * @param name 基础名称 用于与每个级别组合
+ * @param levels 级别数组 表示需要与基础名称组合的各个级别
+ * @returns 返回一个新的字符串数组 每个元素都是基础名称加上一个级别后缀
+ */
 function withLevels(name: string, levels: any[]) {
   return levels.map((suffix) => `${name}-${suffix}`);
 }
 
+/**
+ * 将给定的颜色名称数组转换为对应的CSS变量颜色映射
+ * CSS变量字符串的格式为`var(--vp-c-${name})`
+ * `${name}`是数组中的颜色名称
+ * 
+ * @param names 颜色名称数组 用于生成CSS变量颜色映射
+ * @returns 返回一个对象 key是颜色名称 value是对应的CSS变量字符串
+ */
 function vpColors(names: string[]) {
   const colors: Record<string, string> = {};
   for (const name of names) {
+    // 为每个颜色名称在colors对象中创建一个键值对
     colors[name] = `var(--vp-c-${name})`;
   }
   return colors;
 }
 
+/**
+ * 根据按钮变体生成颜色主题
+ * 
+ * 针对给定的按钮变体
+ * 生成一个包含该变体在不同状态下的颜色主题对象
+ * 为按钮的 文本 背景 边框颜色 定义了 正常 悬停 激活 状态下的CSS变量
+ * 
+ * @param variant 按钮的变体名称
+ * @returns 返回一个对象 key是颜色主题的名称 value是对应的CSS变量
+ */
 function vpButtonColors(variant: string) {
   const colors: Record<string, string> = {};
   for (const target of ['text', 'bg', 'border']) {
+    // 正常状态
     colors[`button-${variant}-${target}`] = `var(--vp-button-${variant}-${target})`;
+    // 悬停状态
     colors[`button-${variant}-hover-${target}`] = `var(--vp-button-${variant}-hover-${target})`;
+    // 激活状态
     colors[`button-${variant}-active-${target}`] = `var(--vp-button-${variant}-active-${target})`;
   }
   return colors;
