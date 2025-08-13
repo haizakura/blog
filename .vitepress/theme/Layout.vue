12843635201f5794cc909b724c22718bd4ec0b07
haizakura <hhbilly99@gmail.com>
2025-08-13 14:19:29 +0900

    feat: add animation effct

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -29,17 +29,21 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onMounted, nextTick } from 'vue';
+import { ref, nextTick, watch, onMounted } from 'vue';
 import { useData } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import Giscus from '@giscus/vue';
+import mediumZoom from 'medium-zoom';
+import { animate, stagger, spring } from 'motion';
 
 import { data as posts } from '../posts.data';
 import type { Post, ThemeConfig } from '.';
+import { springScrollToElement } from './utils';
+
 import LicensingBlock from '@/components/LicensingBlock.vue';
 
 const { Layout } = DefaultTheme;
-const { page, theme, isDark } = useData<ThemeConfig>();
+const { page, theme, frontmatter, isDark } = useData<ThemeConfig>();
 
 onMounted(() => {
   watch(
@@ -51,11 +55,40 @@ onMounted(() => {
       // Random tagline
       const taglineElem = document.querySelector('.tagline');
       if (taglineElem) {
-        taglineElem.innerHTML =
-          theme.value.taglines[Math.floor(Math.random() * theme.value.taglines.length)];
+        taglineElem.innerHTML = theme.value.taglines[Math.floor(Math.random() * theme.value.taglines.length)];
       }
+
+      // Animate home page
+      if (frontmatter.value.layout === 'home') {
+        const homeAnimate = { type: spring, bounce: 0.5, duration: 0.5 };
+
+        animate('.main .name', { opacity: 1, x: [-200, 0] }, { ...homeAnimate });
+        animate('.main .text', { opacity: 1, x: [200, 0] }, { ...homeAnimate });
+        animate('.main .tagline', { y: [50, 0] }, { ...homeAnimate });
+        animate('.main .action', { y: [50, 0] }, { ...homeAnimate, delay: stagger(0.05) });
+      }
+
+      // Animate outline link scrolls
+      document.querySelectorAll<HTMLLinkElement>('.outline-link').forEach((element) => {
+        const id = decodeURIComponent(element.href.split('#')[1]);
+        element.onclick = () => {
+          const target = document.getElementById(id);
+          if (!target) return;
+          springScrollToElement(target);
+        };
+      });
     },
-    { immediate: true }
+    { immediate: true },
+  );
+});
+
+// Trigger on page change
+onMounted(() => {
+  watch(
+    () => page.value.relativePath,
+    async () => {
+      animate('.VPContent', { opacity: [0, 1] }, { ease: 'easeInOut', duration: 0.2 });
+    },
   );
 });
 
@@ -68,6 +101,38 @@ watch(
     const postId = newPath.match(/posts\/(.*)\//)?.[1];
     currentPost.value = postId ? posts.find((post) => post.id === postId) : undefined;
   },
-  { immediate: true }
+  { immediate: true },
 );
+
+// Process markdown image
+const mdImgSelector = '.vp-doc img';
+onMounted(() => {
+  watch(
+    () => currentPost.value,
+    async (post) => {
+      if (!post) return;
+      await nextTick(); // Wait for the DOM to update
+
+      // Append alt text to images
+      document.querySelectorAll(mdImgSelector).forEach((img) => {
+        const alt = img.attributes.getNamedItem('alt');
+        if (!alt) return;
+
+        const node = document.createElement('div');
+        node.classList.add('img-alt');
+        node.innerText = alt.value;
+
+        const parent = img.parentNode!;
+        if (parent.lastChild === img) parent.appendChild(node);
+        else parent.insertBefore(node, img.nextSibling);
+      });
+
+      // Apply medium-zoom
+      mediumZoom(mdImgSelector, {
+        background: 'rgba(0, 0, 0, 0.5)',
+      });
+    },
+    { immediate: true },
+  );
+});
 </script>
